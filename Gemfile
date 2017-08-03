1f580acbcc1e4594a85e0a7d7b4095abb9cd4435
Rebekka Haley <rebekka.haley@gmail.com>
2017-08-03 14:23:30 +0200

    It works woo

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.5'
+# gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
@@ -35,10 +35,18 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Faker for seeding
+gem 'faker'
+# Simple form for making simple forms
+gem 'simple_form'
+# useful comments by Inou .......
+gem 'pry-byebug'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
 end
+
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# spirits
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Rum")
+Ingredient.create(name: "Tequila")
+Ingredient.create(name: "Bourbon")
+Ingredient.create(name: "Brandy")
+Ingredient.create(name: "Scotch whisky")
+
+# liqueurs
+Ingredient.create(name: "Triple Sec")
+Ingredient.create(name: "Maraschino liqueur")
+Ingredient.create(name: "Coffee liqueur")
+Ingredient.create(name: "Crème de Cassis1")
+Ingredient.create(name: "Crème de Cacao")
+
+# vermouths and bitters
+Ingredient.create(name: "Dry Vermouth")
+Ingredient.create(name: "Sweet (red) Vermouth")
+Ingredient.create(name: "Campari")
+Ingredient.create(name: "Angostura Bitters")
+Ingredient.create(name: "Orange Bitters")
+
+# non Alcoholic Syrups
+Ingredient.create(name: "Sugar syrup")
+Ingredient.create(name: "Grenadine")
+
+# garnishes
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Limes and lemons")
+Ingredient.create(name: "Oranges")
+Ingredient.create(name: "Juice")
+Ingredient.create(name: "Salt and pepper")
+Ingredient.create(name: "Sugar and sugar cubes")
+Ingredient.create(name: "Roses Lime Cordial")
+Ingredient.create(name: "Mint leaves")
+
+10.times do
+  cocktail = Cocktail.create(name: Faker::Coffee.blend_name)
+  3.times do
+    Dose.create(description: Faker::Food.measurement, cocktail: cocktail, ingredient: Ingredient.all.sample)
+  end
+end
+
